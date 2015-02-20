6961fd6cba5e03128ec8370e1e5188db3b5d2a89
idelairre <idelairre@gmail.com>
2015-02-20 16:57:09 -0500

    fixing floating parends and getting rid of redundant code.

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,6 @@
 "use strict";
 var dataReference = new Firebase("https://crackling-fire-3909.firebaseio.com/");
 
-$(document).ready(function() {
-
     $("#messageInput").keypress(function(key) {
         if (13 === key.keyCode) {
             var nameInput = $("#nameInput").val();
@@ -11,17 +9,16 @@ $(document).ready(function() {
                 name: nameInput,
                 text: messageInput
             }),
-            $("#messageInput").val("")
+            $("#messageInput").val("");
         }
-    }),
+    });
 
     dataReference.on("child_added", function(child) {
         var message = child.val();
         (message.name, message.text);
-    })
+    });
 
     function displayChatMessage(name, text) {
-        name && text && ($("<div/>").text(text).prepend($("<em/>").text(name + ": ")).appendTo($("#messagesDiv")),
-            $("#messagesDiv")[0].scrollTop = $("#messagesDiv")[0].scrollHeight)
-    }
-});
+        $("<div/>").text(text).prepend($("<em/>").text(name + ": ")).appendTo($("#messagesDiv"));
+        $("#messagesDiv")[0].scrollTop = $("#messagesDiv")[0].scrollHeight;
+    };
