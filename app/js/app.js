270569385a29c09a7e3784390514df2977814714
Ashlinn Brennan <asbren13@gmail.com>
2015-02-20 16:39:49 -0500

    trying again

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+var chatAppRef = new Firebase("https://flickering-inferno-9737.firebaseio.com/");
+$('#messageInput').keypress(function (e) {
+  if (e.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#messageInput').val();
+    chatAppRef.push({name: name, text: text});
+    $('#messageInput').val('');
+  }
+});
+chatAppRef.on('child_added', function(snapshot) {
+  var message = snapshot.val();
+  displayChatMessage(message.name, message.text);
+});
+function displayChatMessage(name, text) {
+  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
