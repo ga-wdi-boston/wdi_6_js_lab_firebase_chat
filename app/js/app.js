f4c237ce9dd93d60f7fc1fc2a96dc258b2cf54ed
idelairre <idelairre@gmail.com>
2015-02-20 16:47:58 -0500

    renamed variables and wrapped the message input in a function.

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+var dataReference = new Firebase("https://crackling-fire-3909.firebaseio.com/");
+
+$(document).ready(function() {
+
+    $("#messageInput").keypress(function(key) {
+        if (13 === key.keyCode) {
+            var nameInput = $("#nameInput").val();
+            var messageInput = $("#messageInput").val();
+            dataReference.push({
+                name: nameInput,
+                text: messageInput
+            }),
+            $("#messageInput").val("")
+        }
+    }),
+
+    dataReference.on("child_added", function(child) {
+        var message = child.val();
+        (message.name, message.text);
+    })
+
+    function displayChatMessage(name, text) {
+        name && text && ($("<div/>").text(text).prepend($("<em/>").text(name + ": ")).appendTo($("#messagesDiv")),
+            $("#messagesDiv")[0].scrollTop = $("#messagesDiv")[0].scrollHeight)
+    }
+});
