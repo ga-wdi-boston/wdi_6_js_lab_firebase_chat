300b0a8191fed8dccb8a53e79a08ae62a5ccf3e2
idelairre <idelairre@gmail.com>
2015-02-20 17:04:49 -0500

    realized I wasn't calling the display chat message function. fixed some floating semi colons and parenthesis.

--- app/js/app.js
+++ app/js/app.js
@@ -5,17 +5,17 @@ var dataReference = new Firebase("https://crackling-fire-3909.firebaseio.com/");
         if (13 === key.keyCode) {
             var nameInput = $("#nameInput").val();
             var messageInput = $("#messageInput").val();
-            dataReference.push({
-                name: nameInput,
-                text: messageInput
-            }),
+              dataReference.push({
+                  name: nameInput,
+                  text: messageInput
+              });
             $("#messageInput").val("");
         }
     });
 
     dataReference.on("child_added", function(child) {
         var message = child.val();
-        (message.name, message.text);
+        displayChatMessage(message.name, message.text);
     });
 
     function displayChatMessage(name, text) {
