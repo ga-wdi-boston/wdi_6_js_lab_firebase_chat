78404e04b4f160aba8f77565eb2b54639add9958
brandonkoo <brandonkoo555@gmail.com>
2015-02-23 12:49:19 -0500

    WIP

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,30 @@
 'use strict';
+
+
 var Firebase = require("firebase");
+
+
+var DataRef = new firebase('https://sizzling-fire-8233.firebaseio.com/');
+
+var name = $('#nameinput').val();
+    var text = $('#textinput').val();
+
+$('msginput').keypress(function(e) {
+
+
+    DataRef.push({name: name, text: text});
+    $('#msginput').val('');
+  }
+});
+
+function displayChatMessage(name, text) {
+
+
+DataRef.on('child_added', function(snapshot) {
+  var msg = snapshot.val();
+  displayChatMsg(msg.name, msg.text);
+});
+
+
+
+};
