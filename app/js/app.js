bb5d3da9f5be5d550e4919d7465d1e981d4b3e2e
Ross <rdegnen@gmail.com>
2015-02-23 09:09:49 -0500

    commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var myDataRef = new firebase('https://yfba621vdky.firebaseio-demo.com/');
+$('messageInput').keypress(function(e) {
+  if (e.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#textInput').val();
+    myDataRef.push({name: name, text: text});
+    $('#messageInput').val('');
+  }
+});
+myDataRef.on('child_added', function(snapshot) {
+  var message = snapshot.val();
+  displayChatMessage(message.name, message.text);
+});
+function displayChatMessage(name, text) {
+  $(<div/>).text(text).prepend($('<em/>').text(name+': ')).appendTo('#messagesDiv'));
+  $('#messagesDiv')[0].scrollTop = $('messageDiv')[0].scrollHeight;
+};
