9683e960f018da75eaba5f2f96aa92c5f6e116d7
Daniel Tyson <danielctyson@gmail.com>
2015-02-23 09:09:14 -0500

    chat app with auto scroll!

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,35 @@
 'use strict';
+
+var myChatRef = new Firebase("https://tyschat.firebaseio.com/");
+
+var $container = $('#container');
+var $messages = $('#messagesDiv');
+var $message = $('#messageInput');
+var $name = $('#nameInput');
+
+
+$message.keypress(function(e) {
+  if (e.keyCode == 13) {
+    var name = $name.val();
+    var message = $message.val();
+    myChatRef.push({name: name, message: message});
+    $message.val('');
+    scrollDown();
+  }
+});
+
+myChatRef.on('child_added', function(snapshot) {
+  var message = snapshot.val();
+  displayChatMessage(message.name, message.message);
+});
+
+function displayChatMessage(name,message) {
+  $('<div/>').text(message).prepend($('<em/>').text(name+': ')).appendTo($messages);
+  $messages[0].scrollTop = $messages[0].scrollHeight;
+
+};
+
+
+function scrollDown() {
+  $container.animate({ scrollTop: $messages.css('height') }, 1000);
+};
